3cba9001772ad6dcf3af0ab445ca997a658703ed
Alex E.T. Snyder <alexetsnyder@gmail.com>
2026-03-07 11:03:31 -0500

    Added Basic Icons for the Inventory Control

--- scenes/gui/items/InventoryControl.cs
+++ scenes/gui/items/InventoryControl.cs
@@ -1,10 +1,14 @@
 using Godot;
+using Quasar.data;
 using Quasar.scenes.systems.items;
 
 namespace Quasar.scenes.gui.items
 {
     public partial class InventoryControl : Control
     {
+        [Export]
+        public Texture2D AtlasTexture { get; set; }
+
         private ItemList _inventoryItemList;
 
         public override void _Ready()
@@ -16,7 +20,22 @@ namespace Quasar.scenes.gui.items
 
         public void Add(Item item)
         {
-            _inventoryItemList.AddItem(item.TileType.ToString());
+            var atlasTexture = CreateAtlasTexture(item);
+
+            _inventoryItemList.AddItem(item.TileType.ToString(), atlasTexture);
+        }
+
+        public AtlasTexture CreateAtlasTexture(Item item)
+        {
+            AtlasTexture atlasTexture = new();
+
+            atlasTexture.Atlas = this.AtlasTexture;
+
+            var atlasCoords = AtlasConstants.GetAtlasCoords(item.TileType);
+
+            atlasTexture.Region = new(atlasCoords.X * 18.0f, atlasCoords.Y * 18.0f, 18.0f, 18.0f);
+
+            return atlasTexture;
         }
     }
 }
